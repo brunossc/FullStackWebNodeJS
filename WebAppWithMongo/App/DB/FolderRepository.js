eb0a1a41c5495637318a92d19e5c1d8814deafc2
Bruno Sena e Silva Correia <brunossc@outlook.com>
2017-04-19 05:06:03 -0300

    removido metodos da base nos filhos nas classe repository

--- WebAppWithMongo/App/DB/FolderRepository.js
+++ WebAppWithMongo/App/DB/FolderRepository.js
@@ -6,33 +6,4 @@ function FolderRepository(dbConn) {
 }
 
 FolderRepository.prototype = Object.create(Repository.prototype);
-
-FolderRepository.prototype.Insert = function (data, cb) {
-    this.BaseInsert(data, cb);
-};
-
-FolderRepository.prototype.Delete = function (filter, cb) {
-    this.BaseDelete(filter, cb);
-};
-
-FolderRepository.prototype.DeleteById = function (id, cb) {
-    this.BaseDeleteById(id, cb);
-};
-
-FolderRepository.prototype.Update = function (filter, set, cb) {
-    this.BaseUpdate(filter, set, cb);
-};
-
-FolderRepository.prototype.GetAll = function (cb) {
-    this.BaseGetAll(cb);
-};
-
-FolderRepository.prototype.Find = function (filter, cb) {
-    this.BaseFind(filter, cb);
-};
-
-FolderRepository.prototype.FindById = function (id, cb) {
-    this.BaseFindById(id, cb);
-};
-
 exports.Object = FolderRepository;
--- WebAppWithMongo/App/DB/Repository.js
+++ WebAppWithMongo/App/DB/Repository.js
@@ -6,37 +6,37 @@ function Repository(table)
     this.table = table;
 }
 
-Repository.prototype.BaseInsert = function (data, cb) {
+Repository.prototype.Insert = function (data, cb) {
 
     this.table.insert(data, { safe: true }, cb);
 };
 
-Repository.prototype.BaseDelete = function (filter, cb) {
+Repository.prototype.Delete = function (filter, cb) {
 
     this.table.remove(filter, true, cb);
 };
 
-Repository.prototype.BaseDeleteById = function (id, cb) {
+Repository.prototype.DeleteById = function (id, cb) {
 
     this.table.deleteOne({ _id: new mongodb.ObjectID(id._id) }, cb);
 };
 
-Repository.prototype.BaseUpdate = function (filter, set, cb) {
+Repository.prototype.Update = function (filter, set, cb) {
 
     this.table.update({ _id: new mongodb.ObjectID(filter._id) }, {$set : set}, { safe: true }, cb);
 };
 
-Repository.prototype.BaseGetAll = function (cb) {
+Repository.prototype.GetAll = function (cb) {
 
     this.table.find().toArray(cb);
 };
 
-Repository.prototype.BaseFind = function (filter, cb) {
+Repository.prototype.Find = function (filter, cb) {
 
     this.table.find(filter).toArray(cb);
 };
 
-Repository.prototype.BaseFindById = function (id, cb) {
+Repository.prototype.FindById = function (id, cb) {
 
     this.table.find({ _id: new mongodb.ObjectID(id._id) }).toArray(cb);
 };
